e40e3cb39d4926f75d7b91823c5bc23e2d3277a9
Ramesh <pishini.ramesh@gmail.com>
2019-12-07 14:30:01 +0100

    Display the UID and Username of the user executing this script

---
+++ demo2.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Display the UID and Username of the user executing this script
+# Display the user is root user or not
+
+# Display the UID
+
+echo "Your UID is ${UID}"
+
+# Display the username
+
+echo "your username is ${USERNAME}"
+
+# Display if root user or not
+if [[ '${UID}' -eq 0 ]]
+then
+    echo 'you are root'
+   else
+    echo 'you are not root'
+fi
