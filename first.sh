f25a3d176c2e5132db0c7db6f5053b7183446517
Ramesh <pishini.ramesh@gmail.com>
2019-12-07 13:45:11 +0100

    variable

--- first.sh
+++ first.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 #This script display varies information on screen
+
+# Display Hello world
 echo 'hello world'
+
+# assign a value to a variable
+
+WORD='script'
+
+#Display that value using the variable
+
+echo "$WORD"
